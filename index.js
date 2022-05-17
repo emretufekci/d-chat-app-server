3b8212de75d45119a92aa7065047a4872b456469
emretufekci <contact@emretufekci.com>
2022-05-17 10:02:06 +0300

    first commit for chat server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+app.use(cors());
+const Gun = require("gun")
+
+const server = require('http').createServer().listen(8765);
+  new Gun({
+    web: server,
+  });
